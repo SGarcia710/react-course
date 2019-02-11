52c51b775090bd9276711edad976eea839f0bac3
SGarcia710 <sgarcia710@outlook.com>
2019-02-11 01:14:32 -0500

    Windows try 2

--- src/widgets/components/modal.js
+++ src/widgets/components/modal.js
@@ -4,7 +4,7 @@ import './modal.css'
 function Modal(props) {
   return(
     <div className="Modal">
-      esto es un modal trabajado en mac
+      A dios le pido
       {props.children}
       <button onClick={props.handleClick}>Cerrar</button>
     </div>
