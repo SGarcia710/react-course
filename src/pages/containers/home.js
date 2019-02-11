962cb44b957480a99664e2c5962f374dddf22c6b
SGarcia710 <sgarcia710@outlook.com>
2019-02-11 01:32:44 -0500

    Errores 2

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -33,7 +33,7 @@ class Home extends Component {
       <HomeLayout>
         <Related/>
         <Categories 
-        categories={this.props.data}
+        categories={this.props.data.categories}
         handleOpenModal={this.handleOpenModal}
         />
         {
