389ab2ae9c158167e8742280c9ae29c8268cbc33
SGarcia710 <sgarcia710@outlook.com>
2019-02-11 01:26:08 -0500

    Last Windows Try

--- src/widgets/components/modal.js
+++ src/widgets/components/modal.js
@@ -4,9 +4,7 @@ import './modal.css'
 function Modal(props) {
   return(
     <div className="Modal">
-      A dios le pido
-      que si me muero sea de amor
-      que si me enamoro se a de voz
+      
       {props.children}
       <button onClick={props.handleClick}>Cerrar</button>
     </div>
